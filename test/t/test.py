d2d7f718a061e36206db51b3e00614e139e86b06
mokasin <mok4sin@googlemail.com>
2010-03-01 12:49:29 +0100

    Added testdata

--- test/t/test.py
+++ test/t/test.py
@@ -8,35 +8,42 @@ import its
 @wvtest
 def test_get_keyword_dbs():
     tmp = its.get_keyword_dbs('testdata/', 'courierimapkeywords/:list')
-    WVPASSEQ(len(tmp), 3)
+    WVPASSEQ(len(tmp), 2)
     
     for t in tmp:
         WVPASS(os.path.isfile(t))
-        WVPASSEQ(t.split('/')[-1:], [':list'])
+        WVPASSEQ(t[-len('courierimapkeywords/:list'):],\
+                                                    'courierimapkeywords/:list')
 
 @wvtest
 def test_parse_courierdb():
-    tmp = its.parse_courierdb('testdata/1/Maildir/courierimapkeywords/:list',
-                                            {'Junk': 'spam', 'nonjunk': 'ham'})
-    WVPASSEQ(len(tmp['spam']), 2)
+    tmp = its.parse_courierdb('testdata/Maildir/courierimapkeywords/:list',
+                             {'spam': ['junk', 'alsojunk'], 'ham': ['nonjunk']})
+    print os.path.realpath('testdata/Maildir/courierimapkeywords/:list')
+    WVPASSEQ(len(tmp['spam']), 4)
     WVPASSEQ(len(tmp['ham']), 1)
     WVPASS('1_mail_spam' in tmp['spam'])
     WVPASS('1_mail_spam_x' in tmp['spam'])
+    WVPASS('1_mail_spam2' in tmp['spam'])
+    WVPASS('1_mail_spam3' in tmp['spam'])
     WVPASS('1_mail_ham' in tmp['ham'])
 
 @wvtest
 def test_get_path_from_filename():
-    tmp = its.get_path_from_filename(os.path.realpath('testdata/1'),
-            its.parse_courierdb('testdata/1/Maildir/courierimapkeywords/:list',
-                                            {'Junk': 'spam', 'nonjunk': 'ham'}))
-    WVPASSEQ(len(tmp['spam']), 2)
+    tmp = its.get_path_from_filename(os.path.realpath('testdata/'),
+            its.parse_courierdb('testdata/Maildir/courierimapkeywords/:list',
+                            {'spam': ['junk', 'alsojunk'], 'ham': ['nonjunk']}))
+    WVPASSEQ(len(tmp['spam']), 4)
     WVPASSEQ(len(tmp['ham']), 1)
-    WVPASS(os.path.realpath('testdata/1/Maildir/mails/1_mail_spam:header') in tmp['spam'])
-    WVPASS(os.path.realpath('testdata/1/Maildir/mails/1_mail_spam_x:xy') in tmp['spam'])
-    WVPASS(os.path.realpath('testdata/1/Maildir/mails/1_mail_ham:random') in tmp['ham'])
+    WVPASS(os.path.realpath('testdata/Maildir/mails/1_mail_spam:header') in tmp['spam'])
+    WVPASS(os.path.realpath('testdata/Maildir/mails/1_mail_spam_x:xy') in tmp['spam'])
+    WVPASS(os.path.realpath('testdata/Maildir/mails/1_mail_spam2:xyz') in tmp['spam'])
+    WVPASS(os.path.realpath('testdata/Maildir/mails/1_mail_spam3:xyz') in tmp['spam'])
+
+    WVPASS(os.path.realpath('testdata/Maildir/mails/1_mail_ham:random') in tmp['ham'])
 
 @wvtest
 def test_parse_mail_header():
-    tmp = its.parse_mail_header('testdata/1/Maildir/mails/1_mail_spam:header')
+    tmp = its.parse_mail_header('testdata/Maildir/mails/1_mail_spam:header')
     WVPASSEQ(tmp[0], 'from@me.com')
     WVPASSEQ(tmp[1], 'The subject')
